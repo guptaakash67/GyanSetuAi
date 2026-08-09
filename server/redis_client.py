de9395eed2a71e35a55c6354ae728c88129fc4b3
Akash Gupta <101046917+guptaakash67@users.noreply.github.com>
2026-08-09 20:25:03 +0530

    cache_otp

--- server/redis_client.py
+++ server/redis_client.py
@@ -1,8 +1,20 @@
+import os
 import redis
+from dotenv import load_dotenv
 
-redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
-    db=0,
-    decode_responses=True
+load_dotenv()
+
+REDIS_URL = os.getenv("REDIS_URL")
+
+redis_client = redis.from_url(
+    REDIS_URL,
+    decode_responses=True,  # returns strings instead of bytes
+    ssl_cert_reqs=None,     # required for Upstash TLS
 )
+
+def test_connection():
+    try:
+        redis_client.ping()
+        print("Redis connected successfully")
+    except Exception as e:
+        print(f"Redis connection failed: {e}")
